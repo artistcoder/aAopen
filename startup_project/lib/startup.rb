fe3d2b428645cd22d5749e5d4432feb96b0631a8
your name here <jennaleelumarie@gmail.com>
2019-06-30 21:35:37 -0600

    complete startup project

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -29,7 +29,7 @@ class Startup
         @salaries.keys.include?(title)
     end
 
-    def hire(employee_name, title)
+    def hire(employee_name, title)5
         if valid_title?(title)
             @employees << Employee.new(employee_name, title)
         else
@@ -37,6 +37,56 @@ class Startup
         end
     end
 
+    def size
+        @employees.length
+    end
+
+    def pay_employee(employee)
+        salary = @salaries[employee.title]
+        if @funding >= salary
+            employee.pay(salary)
+            @funding -= salary
+        else
+            raise ArgumentError.new("error: not enough funding")
+        end
+    end
+
+    def payday
+        @employees.each{|employee| pay_employee(employee)}
+    end
+
+    def average_salary
+        total_sal = 0
+        @employees.each do |employee|
+            total_sal += @salaries[employee.title]
+        end
+        total_sal/@employees.length
+    end
+
+    def close
+        @employees = []
+        @funding = 0
+    end
+
+    def acquire(startup)
+        @funding += startup.funding
+        @employees += startup.employees
+        startup.salaries.each do |title, salary|
+            if !@salaries.keys.include?(title)
+                @salaries[title] = salary
+            end
+        end
+        startup.close
+    end
+
+    def >(startup)
+        if self.funding > startup.funding
+            true
+        else
+            false
+        end
+
+    end
 
 
 end
