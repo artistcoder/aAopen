032aa970691a05103ae1393dd0a711b448b6e74f
your name here <jennaleelumarie@gmail.com>
2019-07-02 10:35:38 -0600

    first completion of mastermind project

---
+++ mastermind_project/lib/code.rb
@@ -0,0 +1,80 @@
+class Code
+  POSSIBLE_PEGS = {
+    "R" => :red,
+    "G" => :green,
+    "B" => :blue,
+    "Y" => :yellow
+  }
+
+  attr_reader :pegs
+
+  def self.valid_pegs?(arr)
+    arr.all?{|char| POSSIBLE_PEGS.has_key?(char.upcase)}
+  end
+
+  def initialize(char_arr)
+    raise ArgumentError.new('Error: pegs are not valid') if !Code.valid_pegs?(char_arr)  
+    @pegs = char_arr.map{|char| char.upcase} 
+  end
+
+  def self.random(len)
+    peg_arr = []
+    len.times{peg_arr << POSSIBLE_PEGS.keys.sample}
+    Code.new(peg_arr)
+  end
+
+  def self.from_string(peg_string)
+    Code.new(peg_string.split(""))
+  end
+
+  def [](idx)
+    @pegs[idx]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def num_exact_matches(code)
+    # return the number of pegs in the guess that are the correct color and position as @pegs
+    count = 0
+    code.pegs.each_with_index do |peg_1,idx_1|
+      @pegs.each_with_index do |peg_2, idx_2|
+        if idx_1 == idx_2 && peg_1 == peg_2
+          count += 1
+        end
+      end
+    end
+    count
+  end
+
+  def num_near_matches(code)
+    matches = 0
+    code_hash = Hash[POSSIBLE_PEGS.keys.product([0])]
+    pegs_hash = Hash[POSSIBLE_PEGS.keys.product([0])]
+    code.pegs.each do |peg|
+      code_hash[peg] += 1
+    end
+    @pegs.each do |peg2|
+      pegs_hash[peg2] += 1
+    end
+    code_hash.each do |k,v|
+      if code_hash[k] < pegs_hash[k]
+        matches += code_hash[k]
+      else
+        matches += pegs_hash[k]
+      end
+    end
+    matches - num_exact_matches(code)
+  end
+
+  def ==(code)
+    self.pegs == code.pegs
+  end
+
+
+  # ABCB  code a 1 b1 c 1
+  # BBCA pegs b2 c1
+  # 2 EXACT. 1 NEAR
+
+end
