52d62bd0b0fbf3d1e729f891c868e120ccec9a15
your name here <jennaleelumarie@gmail.com>
2019-08-08 12:25:29 -0700

    Segregate validating a user's guess and related error message into 2 new methods; removed from guess method

--- Ghost/Player.rb
+++ Ghost/Player.rb
@@ -10,12 +10,24 @@ class Player
     def guess
         puts "#{self.name} enter a letter: "
         letter = gets.chomp
+        validate_guess(letter)
+    end
+
+    def validate_guess(letter)
         if letter.length > 1 || !@alphabet.include?(letter.downcase)
-            puts "must enter a single letter"
+            invalid_entry_error
             self.guess
         else
-            letter.downcase
+            return letter.downcase
         end
     end
+
+    def invalid_entry_error
+        puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        puts "must enter a single letter"
+        puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    end
+    
+
 
 end
