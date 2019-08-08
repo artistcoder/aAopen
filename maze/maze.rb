bfad060b50e6b1655548dc21a07399476bc91450
your name here <jennaleelumarie@gmail.com>
2019-08-08 15:20:43 -0700

    Find location of end point

--- maze/maze.rb
+++ maze/maze.rb
@@ -9,23 +9,25 @@ class Maze
 # add "stuck spots to ''do not go array'''"
 #< if stuck, go back 1 position
 # note prior position to avoid, avoid and go to next available position..loop>
-attr_reader :maze_map, :map_height, :map_width
+    attr_reader :maze_map, :map_height, :map_width, :s_loc, :e_loc
 
-def initialize(maze_file)
-    #maze map is a 2x2 array
-    @maze_map = IO.readlines(maze_file, chomp: true)
-    @map_height =  @maze_map.length
-    @map_width =  @maze_map[0].length
-    # @loc = find_s
-end
+    def initialize(maze_file)
+        #maze map is a 2x2 array
+        @maze_map = IO.readlines(maze_file, chomp: true)
+        @map_height =  @maze_map.length
+        @map_width =  @maze_map[0].length
+        find_start_and_end_points
+    end
 
-def find_s
-    @maze_map.each_with_index do |row, row_idx|
-        row.each_char.with_index do |char, col_idx|
-            return row_idx, col_idx if char == "S"                
+    def find_start_and_end_points
+        @maze_map.each_with_index do |row, row_idx|
+            row.each_char.with_index do |char, col_idx|
+                @s_loc = row_idx, col_idx if char == "S" 
+                @e_loc = row_idx, col_idx if char == "E" 
+            end
         end
     end
-end
+    
 
 end
 
@@ -33,8 +35,11 @@ end
 play_maze = Maze.new("example_1_maze.txt")
 p play_maze.map_height
 p play_maze.map_width
-r,c = play_maze.find_s
-p [r,c]
+p play_maze.s_loc
+p play_maze.e_loc
+
+# r,c = play_maze.find_s
+# p [r,c]
 #puts play_maze.maze_map[6][1]
 
 # if $PROGRAM_NAME == __FILE__
