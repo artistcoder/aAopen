9e1db76d5987d0c6395e3dde1618c7ff5e3aa410
your name here <jennaleelumarie@gmail.com>
2019-08-11 21:25:59 -0600

    Display path from S to E

--- maze/maze.rb
+++ maze/maze.rb
@@ -1,24 +1,13 @@
 class Maze
-# takes in a maze text file, and finds a path between the
-# S and E locations. Must respect the * walls
 
-#find S starting point
-# look at surrounding spaces (Up, Down, Left, Right)
-# move to first availeble surrounding space.
-# continue until stuck
-# add "stuck spots to ''do not go array'''"
-#< if stuck, go back 1 position
-# note prior position to avoid, avoid and go to next available position..loop>
-    attr_reader :maze_map, :map_height, :map_width, :s_loc, :e_loc, :current_loc
+    attr_reader :maze_map, :s_loc, :e_loc, :current_loc, :path
 
     def initialize(maze_file)
-        #maze map is a 2x2 array
         @maze_map = IO.readlines(maze_file, chomp: true)
-        @map_height =  @maze_map.length
-        @map_width =  @maze_map[0].length
         find_start_and_end_points
         @current_loc = @s_loc
-
+        @path = []
+        @dead_ends = []
     end
 
     def find_start_and_end_points
@@ -30,42 +19,65 @@ class Maze
         end
     end
 
-    def up
-        @current_loc[0] = @current_loc[0] - 1
+    def make_path
+        return display_path if up == @e_loc || down == @e_loc || left == @e_loc || right == @e_loc
+        if move(up) || move(right) || move(down) || move(left)
+        else
+            @dead_ends << @path.pop
+            @current_loc = @path[-1] 
+        end
+        make_path
     end
 
+    def move(location)
+        if self[location] == " " && not_a_dead_end(location) && !path.include?(location)
+            @current_loc = location
+            @path << @current_loc
+            return true
+        end
+        return false
+    end
+
+
+    def display_path
+        @path.each do |coordinate|
+            self[coordinate] = "+"
+        end
+        puts @maze_map
+    end
+
+
+    def not_a_dead_end(direction)
+        return false if @dead_ends.include?(direction)
+        true
+    end
+
+    def [](pos)
+        r,c = pos
+        @maze_map[r][c]
+    end
+
+    def []=(pos,value)
+        r,c = pos
+        @maze_map[r][c] = value
+    end
+
+    def up
+        [@current_loc[0]-1, @current_loc[1]]
+    end
     def down
-        @current_loc[0] = @current_loc[0] + 1
+        [@current_loc[0]+1, @current_loc[1]]
     end
     def left
-        @current_loc[1] = @current_loc[1] - 1
+        [@current_loc[0], @current_loc[1]-1]
     end
     def right
-        @current_loc[1] = @current_loc[1] + 1
-
+        [@current_loc[0], @current_loc[1]+1]
     end
-
-    
 
 end
 
 
-play_maze = Maze.new("example_1_maze.txt")
-# p play_maze.map_height
-# p play_maze.map_width
-# p play_maze.s_loc
-# p play_maze.e_loc
-p play_maze.current_loc
-play_maze.right
-p play_maze.current_loc
-
-
-
-# r,c = play_maze.find_s
-# p [r,c]
-#puts play_maze.maze_map[6][1]
-
-# if $PROGRAM_NAME == __FILE__
-#     Maze.new("example_1_maze_map.txt")
-# end
-  
+maze = Maze.new("example_1_maze.txt")
+maze.make_path
+#maze.display_path
