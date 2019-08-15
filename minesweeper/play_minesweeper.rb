da4c110a64ec03441fb4d45d7e9fd29084a635a0
your name here <jennaleelumarie@gmail.com>
2019-08-15 15:41:38 -0700

    Print location of bomb if hit

--- minesweeper/play_minesweeper.rb
+++ minesweeper/play_minesweeper.rb
@@ -7,16 +7,21 @@ until minesweeper.game_over? || minesweeper.won?
     system "clear"
     # minesweeper.render2
     minesweeper.render
-    puts "Enter location to reveal: "
+    puts "Enter location to reveal cell (avoid the bombs!): "
     user_guess = gets.chomp
-    minesweeper.update_board(user_guess)
+    x,y = user_guess.split(/[\s,]/).map(&:to_i)
+    if (x>9 || x<1) || (y>9 || y<1)
+        puts "Must enter a valid location"
+    else
+        minesweeper.update_board(user_guess)
+    end 
 end
 
 system "clear"
 minesweeper.reveal_bombs
 minesweeper.render
 if minesweeper.game_over? 
-    puts "Game Over - you hit a bomb"
+    puts "Game Over - you hit a bomb at location #{x},#{y}"
 else
     puts "You Won!"
 end
