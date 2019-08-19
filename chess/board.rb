d2f0c0ce0b0e5c52087e79eaab5303a362da96b5
your name here <jennaleelumarie@gmail.com>
2019-08-18 20:55:38 -0700

    Move piece on chess board

--- chess/board.rb
+++ chess/board.rb
@@ -1,21 +1,46 @@
 class Board
+    require_relative 'piece'
 
     def initialize
-        @board = Arrary.new(8){Array.new(8)}
+        @board = Array.new(8){Array.new(8)}
         @board.each_with_index do |row,idx|
-            row.each do |element|
-                if idx < 2 || idx > 6
-                    element = Piece.new
+            row.each_with_index do |element, idx2|
+                if idx < 2 || idx > 5
+                    @board[idx][idx2] = Piece.new
                 else
-                    element = nil
+                    @board[idx][idx2] = nil
                 end
             end
         end
     end
 
+    def [](pos)
+        r,c = pos
+        @board[r][c]
+    end
+    def []=(pos,value)
+        r,c = pos
+        @board[r][c] = value
+    end
+
     def move_piece(start_pos, end_pos)
+        piece = self[start_pos]
+        if self[start_pos] == nil
+            raise "Error: There is no piece at this location"
+        elsif !self.valid_pos?(end_pos)
+            raise "Error: The new location is not valid"
+        else
+            piece.pos << end_pos
+            self[start_pos] = nil
+            self[end_pos] = piece
+       end
+    end
+
+    def valid_pos?(pos)
     end
 
 
 end
 
+board = Board.new
+p board[3][3]
