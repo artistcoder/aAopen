0f69ba7284deceef4fa684fc389b278ba565636e
your name here <jennaleelumarie@gmail.com>
2019-09-05 13:32:54 -0700

    Review difficult queries

--- sql_zoo_active_record/movie_buff/03_queries.rb
+++ sql_zoo_active_record/movie_buff/03_queries.rb
@@ -1,6 +1,10 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .where('actors.name = all(?:those_actors)')
 
 end
 
