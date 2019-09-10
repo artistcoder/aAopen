4b72fe1b053970451f5c3ae61a46f0aca98f44c1
your name here <jennaleelumarie@gmail.com>
2019-09-10 14:14:02 -0700

    Initialize sql object

--- build_active_record/lib/00_attr_accessor_object.rb
+++ build_active_record/lib/00_attr_accessor_object.rb
@@ -1,5 +1,19 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |name|
+      getter_method = name.to_s
+      setter_method = name.to_s + "="
+      instance_variable = "@" + name.to_s
+
+      define_method(getter_method) {
+          instance_variable_get(instance_variable)
+        }
+      define_method(setter_method) do |value|
+          instance_variable_set(instance_variable,value)
+      end
+      
+    end
+   
   end
 end
--- build_active_record/lib/01_sql_object.rb
+++ build_active_record/lib/01_sql_object.rb
@@ -6,17 +6,42 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
+    return @columns if @columns
+    column_arr = DBConnection.execute2(<<-SQL).first
+        SELECT
+          *
+        FROM
+          #{self.table_name}
+        LIMIT
+          0
+        SQL
+      
+      column_arr.map!(&:to_sym)
+      @columns = column_arr
   end
 
   def self.finalize!
+
+    self.columns.each do |col|
+      define_method(col) {
+        self.attributes[col]
+      }
+      define_method("#{col}=") do |value|
+        self.attributes[col] = value
+      end
+
+    end
+
   end
 
   def self.table_name=(table_name)
-    # ...
+    # ...4W
+    @table_name = table_name
   end
 
   def self.table_name
     # ...
+    @table_name || self.to_s.tableize
   end
 
   def self.all
@@ -33,10 +58,22 @@ class SQLObject
 
   def initialize(params = {})
     # ...
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+
+      if self.class.columns.include?(attr_name)
+        send("#{attr_name}=",value)
+      else
+        raise ArgumentError.new "unknown attribute '#{attr_name}'"
+      end
+
+    end
+
   end
 
   def attributes
     # ...
+    @attributes ||= {}
   end
 
   def attribute_values
