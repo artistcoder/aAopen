40bc94d2e88fbe7e0ed026d1a1ea88d347683660
your name here <jennaleelumarie@gmail.com>
2019-10-03 15:31:04 -0700

    In progress work on router

--- rails_lite/lib/router.rb
+++ rails_lite/lib/router.rb
@@ -2,10 +2,12 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern, @http_method, @controller_class, @action_name = pattern, http_method, controller_class, action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    (@pattern =~ req.path && @http_method.to_s.upcase == req.request_method)
   end
 
   # use pattern to pull out route params (save for later?)
@@ -18,10 +20,12 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
@@ -32,10 +36,14 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) { add_route(pattern, http_method, controller_class, action_name) }
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.each do |route|
+      return route if route.matches?(req)
+    end
   end
 
   # either throw 404 or call run on a matched route
